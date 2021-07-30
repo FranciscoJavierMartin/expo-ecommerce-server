486eb2de5e8c8d610dfb7324e723ffd8c96b6f88
Francisco <javiermartincuenca@gmail.com>
2021-07-30 21:36:04 +0200

    WIP

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1ca43849b32ea6e9b55042e96443e6b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '327edac8-4057-4363-9373-65b5458e6e8f'
+};
